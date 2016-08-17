6c9c9ce91d8b6987ef709e7b995cb34aeaaa900a
codemuhammed <codemuhammed@gmail.com>
2016-08-17 13:32:13 +0100

    Update comment for delete

--- noteApp.js
+++ noteApp.js
@@ -98,7 +98,12 @@ class NotesApplication {
       }
     }
 
-    
+    /**
+     *This method takes the id of a note  and deletes the note content of that id
+  	 * Argument passed in must be strictly of integer type
+     *
+  	 * @params integer note_id
+     */
     deleteNote(note_id) {
       //check to see if note_id is within bounds
       if(Math.abs(note_id) < this.notes_list.length) {
