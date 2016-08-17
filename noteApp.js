1b8dca9d4aca53326cb232d1341d8e48c4c001b8
codemuhammed <codemuhammed@gmail.com>
2016-08-17 10:47:43 +0100

    This is barebones version of the application with all methods defined

---
+++ noteApp.js
@@ -0,0 +1,77 @@
+'use strict';
+
+/**
+ *
+ *
+ *
+ * {} []
+ */
+
+ class NotesApplication {
+
+ 	/**
+	 *
+	 *
+	 *
+	*/
+    constructor(author) {
+        this.authorName = author;
+        this.notesList = [];
+    }
+
+    /**
+	 *
+	 *
+	 *
+	*/
+    create(note_content) {
+        console.log(this.note_content);
+    }
+
+    /**
+	 *
+	 *
+	 *
+	*/
+    listNotes() {
+
+    }
+
+
+    /**
+	 *
+	 *
+	 *
+	*/
+    get(note_id) {
+
+    }
+
+    /**
+	 *
+	 *
+	 *
+	*/
+    search(search_text) {
+
+    }
+
+    /**
+	 *
+	 *
+	 *
+	*/
+    delete(note_id) {
+
+    }
+
+    /**
+	 *
+	 *
+	 *
+	*/
+    edit(note_id, new_content) {
+
+    }
+
+ }
